1e6984644aa4ebf92a1c2ea9d1142d73da81c5d7
Sharon Watkins <swatkins876@gmail.com>
2019-11-08 16:19:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,20 +1,20 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books 
-  JOIN series ON books.series_id = series.id 
+  "SELECT books.title, books.year FROM books
+  JOIN series ON books.series_id = series.id
   WHERE series.id = 1 ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters 
-  ORDER BY length(motto) DESC 
+  "SELECT name, motto FROM characters
+  ORDER BY length(motto) DESC
   LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) AS num_species FROM characters 
-  GROUP BY species 
-  ORDER BY num_species DESC 
+  "SELECT species, COUNT(species) AS num_species FROM characters
+  GROUP BY species
+  ORDER BY num_species DESC
   LIMIT 1;"
 end
 
